9cd778f9fd69d819f4175da2b99f442450bec41e
MartinClement <martinclementjules@gmail.com>
2023-11-10 19:28:15 +0100

    move form into dedicated component

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
 import BuzzupIcon from "../../components/icons/BuzzupIcon.vue";
-import BaseButton from "../../components/button/BaseButton.vue";
-import BaseInput from "../../components/form/BaseInput.vue";
-import UserIcon from "../../components/icons/UserIcon.vue";
+import AnonymousForm from "./components/AnonymousForm.vue";
 
-import { ref } from "vue";
-
-const username = ref('');
 </script>
 
 <template>
-  <div class="border-4 border-gray-950 border-opacity-10 rounded-lg p-8 shadow-container bg-purple-950 bg-opacity-25 flex flex-col gap-2">
-    <BuzzupIcon />
-    <BaseInput v-model="username" name="username_input"/>
-    <BaseButton class="m-auto">
-      <template #before-slot><UserIcon /></template>Login
-    </BaseButton>
+  <div class="border-4 border-gray-950 border-opacity-10 rounded-lg p-8 shadow-container  flex flex-col gap-2">
+    <BuzzupIcon class="m-auto"/>
+    <AnonymousForm :onSubmit="() => ({})"></AnonymousForm>
   </div>
 </template>
---
+++ src/pages/home/components/AnonymousForm.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseButton from "../../../components/button/BaseButton.vue";
+import BaseInput from "../../../components/form/BaseInput.vue";
+import PlayIcon from "../../../components/icons/PlayIcon.vue";
+
+interface AnonymousFormProps {
+  onSubmit?: (username: string) => any,
+}
+
+withDefaults(defineProps<AnonymousFormProps>(), {
+  onSubmit: () => ({}),
+});
+
+const username = ref('');
+</script>
+
+<template>
+  <form @submit.prevent class="flex flex-col gap-4 bg-purple-950 bg-opacity-25 p-8 rounded-md">
+    <div class="flex flex-row gap-4 items-center">
+      <img src="https://garticphone.com/images/avatar/5.svg" alt="avatar" class="h-[150px] w-[150px] rounded-full border-4 border-white"/>
+      <div class="flex flex-col gap-2 flex-2 max-w-[300px]">
+        <h3 class="text-2xl font-bold text-white text-center">Choisis un surnom et un avatar</h3>
+        <BaseInput v-model="username" name="username_input"/>
+      </div>
+    </div>
+    <BaseButton class="m-auto">
+      <template #before-slot><PlayIcon /></template>DEMARRER
+    </BaseButton>
+  </form>
+</template>
